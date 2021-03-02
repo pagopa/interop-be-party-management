a4df3a7a5e4dadec058eb8c72e6a783d40ff90ab
Stefano Perazzolo <stefanoperazzolo@Stefanos-MacBook-Pro.local>
2021-03-02 10:27:27 +0100

    PIN-29: complete

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/api/impl/PartyApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/api/impl/PartyApiMarshallerImpl.scala
@@ -7,12 +7,50 @@ import it.pagopa.pdnd.interop.uservice.partymanagement.api.PartyApiMarshaller
 import it.pagopa.pdnd.interop.uservice.partymanagement.model.{Credential, ErrorResponse, Person}
 import spray.json._
 
+import java.util.UUID
+import scala.util.{Failure, Success, Try}
+
 class PartyApiMarshallerImpl extends PartyApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
-  override implicit def fromEntityUnmarshallerPerson: FromEntityUnmarshaller[Person] = ???
 
   override implicit def toEntityMarshallerCredential: ToEntityMarshaller[Credential] = ???
 
   override implicit def toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse] = ???
 
-  override implicit def toEntityMarshallerPerson: ToEntityMarshaller[Person] = ???
+  implicit val uuidFormat: JsonFormat[UUID] =
+    new JsonFormat[UUID] {
+      override def write(obj: UUID): JsValue = JsString(obj.toString)
+
+      override def read(json: JsValue): UUID = json match {
+        case JsString(s) =>
+          Try(UUID.fromString(s)) match {
+            case Success(result) => result
+            case Failure(exception) =>
+              deserializationError(s"could not parse $s as Joda LocalDateTime", exception)
+          }
+        case notAJsString =>
+          deserializationError(s"expected a String but got a ${notAJsString.compactPrint}")
+      }
+    }
+
+  implicit object PersonFormat extends RootJsonFormat[Person] {
+    override def write(person: Person): JsValue = JsObject(
+      "id"         -> JsString(person.id.toString),
+      "name"       -> JsString(person.name),
+      "surname"    -> JsString(person.surname),
+      "fiscalCode" -> JsString(person.fiscalCode)
+    )
+
+    override def read(json: JsValue): Person = {
+      json.asJsObject.getFields("id", "name", "surname", "fiscalCode") match {
+        case Seq(JsString(id), JsString(name), JsString(surname), JsString(fiscalCode)) =>
+          Person(UUID.fromString(id), name, surname, fiscalCode)
+        case _ => deserializationError("Person expected")
+      }
+    }
+
+  }
+
+  override implicit def toEntityMarshallerPerson: ToEntityMarshaller[Person] = sprayJsonMarshaller[Person]
+
+  override implicit def fromEntityUnmarshallerPerson: FromEntityUnmarshaller[Person] = sprayJsonUnmarshaller[Person]
 }
--- src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/api/impl/PartyApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/api/impl/PartyApiServiceImpl.scala
@@ -6,35 +6,9 @@ import it.pagopa.pdnd.interop.uservice.partymanagement.api.PartyApiService
 import it.pagopa.pdnd.interop.uservice.partymanagement.model.{Credential, ErrorResponse, Person}
 
 class PartyApiServiceImpl extends PartyApiService {
-  override def createPerson200(responsePerson: Person)(implicit
-    toEntityMarshallerPerson: ToEntityMarshaller[Person]
-  ): Route = ???
-
-  override def getCredentialById200(responseCredential: Credential)(implicit
-    toEntityMarshallerCredential: ToEntityMarshaller[Credential]
-  ): Route = super.getCredentialById200(responseCredential)
-
-  override def getCredentialById400(responseErrorResponse: ErrorResponse)(implicit
-    toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = ???
-
-  override def getCredentialById404(responseErrorResponse: ErrorResponse)(implicit
-    toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = ???
-
-  override def getPersonByFiscalCode200(responsePerson: Person)(implicit
-    toEntityMarshallerPerson: ToEntityMarshaller[Person]
-  ): Route = ???
-
-  override def getPersonByFiscalCode400(responseErrorResponse: ErrorResponse)(implicit
-    toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = ???
-
-  override def getPersonByFiscalCode404(responseErrorResponse: ErrorResponse)(implicit
-    toEntityMarshallerErrorResponse: ToEntityMarshaller[ErrorResponse]
-  ): Route = ???
-
-  override def createPerson(person: Person)(implicit toEntityMarshallerPerson: ToEntityMarshaller[Person]): Route = ???
+  override def createPerson(person: Person)(implicit toEntityMarshallerPerson: ToEntityMarshaller[Person]): Route = {
+    createPerson200(person)
+  }
 
   override def getPersonByFiscalCode(fiscalCode: String)(implicit
     toEntityMarshallerPerson: ToEntityMarshaller[Person],
