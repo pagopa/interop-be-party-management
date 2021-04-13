80ff19ccf8adc70c0f2d4144e56119b7638526d7
Stefano Perazzolo <stefanoperazzolo@ip-192-168-1-232.eu-central-1.compute.internal>
2021-04-13 17:07:21 +0200

    undeploy only if necessary

--- kubernetes/undeploy.sh
+++ kubernetes/undeploy.sh
@@ -9,9 +9,13 @@ SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 
 NAMESPACE=$(source $SCRIPT_PATH/config; echo $name)
+CHECK=$(kubectl get namespaces | grep $NAMESPACE | awk -F' ' '{ print $1 }')
+echo $NAMESPACE
+echo $CHECK
+if  ([ -h "${CHECK}" ]) then
+  $SCRIPT_PATH/templater.sh $SCRIPT_PATH/deployment.yaml.template -s -f $SCRIPT_PATH/config > $SCRIPT_PATH/deployment.yaml
 
-$SCRIPT_PATH/templater.sh $SCRIPT_PATH/deployment.yaml.template -s -f $SCRIPT_PATH/config > $SCRIPT_PATH/deployment.yaml
+  #kubectl delete -f $SCRIPT_PATH/deployment.yaml
 
-kubectl delete -f $SCRIPT_PATH/deployment.yaml
-
-kubectl delete namespace $NAMESPACE && rm -rf $SCRIPT_PATH/deployment.yaml
+  # kubectl delete namespace $NAMESPACE && rm -rf $SCRIPT_PATH/deployment.yaml
+fi
