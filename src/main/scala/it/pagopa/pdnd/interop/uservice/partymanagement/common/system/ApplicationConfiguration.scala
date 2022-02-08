00d7b0e2f939b1c3dc1790e69d871ce93ae6c1ba
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-02-08 14:55:35 +0100

    ApplicationConfiguration uses lazy val

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partymanagement/common/system/ApplicationConfiguration.scala
@@ -7,14 +7,14 @@ import scala.jdk.CollectionConverters.ListHasAsScala
 object ApplicationConfiguration {
   lazy val config: Config = ConfigFactory.load()
 
-  def serverPort: Int = config.getInt("uservice-party-management.port")
+  lazy val  serverPort: Int = config.getInt("uservice-party-management.port")
 
-  def tokenValidityHours: Long = config.getLong("uservice-party-management.token-validity-hours")
+  lazy val  tokenValidityHours: Long = config.getLong("uservice-party-management.token-validity-hours")
 
-  def jwtAudience: Set[String] = config.getStringList("uservice-party-management.jwt.audience").asScala.toSet
+  lazy val  jwtAudience: Set[String] = config.getStringList("uservice-party-management.jwt.audience").asScala.toSet
 
-  def storageContainer: String = config.getString("uservice-party-management.storage.container")
+  lazy val  storageContainer: String = config.getString("uservice-party-management.storage.container")
 
-  def contractPath: String = config.getString("uservice-party-management.storage.contract-path")
+  lazy val  contractPath: String = config.getString("uservice-party-management.storage.contract-path")
 
 }
