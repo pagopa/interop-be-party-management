21b775e3811a13d5b91dbb230a1c3a985eaa9731
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2022-07-12 16:30:02 +0200

    Update kafka-manager/src/main/scala/it/pagopa/interop/commons/queue/kafka/impl/KafkaPublisherImpl.scala
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- kafka-manager/src/main/scala/it/pagopa/interop/commons/queue/kafka/impl/KafkaPublisherImpl.scala
+++ kafka-manager/src/main/scala/it/pagopa/interop/commons/queue/kafka/impl/KafkaPublisherImpl.scala
@@ -30,7 +30,7 @@ class KafkaPublisherImpl(
   private val sendProducer = SendProducer(producerSettings)(system.toClassic)
 
   def send[T](message: T)(implicit messageSerializer: JsonWriter[T]): Future[String] = {
-    val messageString  = message.toJson.toString
+    val messageString  = message.toJson.compactPrint
     val producerRecord = new ProducerRecord[String, String](topic, messageString)
     val result         = sendProducer.send(producerRecord).map { recordMetadata =>
       logger.debug("Published message [{}] to topic/partition {}/{}", messageString, topic, recordMetadata.partition)
