156ba2d4644c7498d2853970f39112694b71f857
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2022-09-27 15:15:20 +0200

    Update src/main/scala/it/pagopa/interop/partymanagement/model/party/PersistedPartyRelationship.scala
    Co-authored-by: Alessio Gallitano <25105748+galales@users.noreply.github.com>

--- src/main/scala/it/pagopa/interop/partymanagement/model/party/PersistedPartyRelationship.scala
+++ src/main/scala/it/pagopa/interop/partymanagement/model/party/PersistedPartyRelationship.scala
@@ -54,7 +54,7 @@ final case class PersistedPartyRelationship(
     id = id,
     to = to,
     product = product.toRelationshipProduct.id,
-    digitalAddress = institutionUpdate.map(i => i.toInstitutionUpdate).get.digitalAddress.getOrElse("")
+    digitalAddress = institutionUpdate.map(_.toInstitutionUpdate.digitalAddress).getOrElse("")
   )
 }
 
